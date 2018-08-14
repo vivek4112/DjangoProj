d576424a09fd7020d13772bd1c244450bcc90920
Vivek <18182313+vivek4112@users.noreply.github.com>
2018-08-14 13:46:44 -0400

    readbooks view.py changes

--- readbooks/views.py
+++ readbooks/views.py
@@ -14,17 +14,33 @@ def book(request):
     
         name = request.POST["name"]
         search_key = request.POST["search_key"]
+        err_msg = {
+           "message" : 'Book not Found!'
+        }
 
         key = "5JCTgW3KWZrZ302j2ouhQ"
 
-        res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                       params={"key": key, "isbns": name})
-        if res.status_code != 200:
-           raise Exception("ERROR: API request unsuccessful.")
-        data = res.json()
-        data = data["books"]
-        avg_rat = data[0]['average_rating']  
-        book = books.objects.get(isbn__exact=name)
+        
+        
+        
+        
+        if search_key == "isbn":
+            if books.objects.filter(isbn=name).count() == 0:
+                return render(request, "readbooks/error.html",err_msg)
+            else:
+                book = books.objects.get(isbn__exact=name)
+                res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": key, "isbns": book.isbn})
+                data = res.json()
+                data = data["books"]
+                avg_rat = data[0]['average_rating']
+                if res.status_code != 200: 
+                   return render(request, "readbooks/error.html",err_msg)
+        else:
+            return render(request, "readbooks/error.html",err_msg)             
+                      
+
+        
         user = request.user.id
         form = forms.CreateReview()
         context = {
